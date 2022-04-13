9dd83a02019eee70928964282617acc3a1b5358d
uw0606 <103592210+uw0606@users.noreply.github.com>
2022-04-14 01:23:33 +0900

    Add files via upload

---
+++ AKIRA/FreedomeUWA-2/js/main.js
@@ -0,0 +1,83 @@
+'use strict';
+
+// タブメニュー
+{
+  const menuItems = document.querySelectorAll('.tab li a');
+  const contents = document.querySelectorAll('.content');
+  const homeclick = document.getElementById('homeclick');
+
+  menuItems.forEach(clickedItem => {
+    clickedItem.addEventListener('click', e => {
+      e.preventDefault();
+
+      menuItems.forEach(item => {
+        item.classList.remove('active');
+      });
+      clickedItem.classList.add('active');
+
+      contents.forEach(content => {
+        content.classList.remove('active');
+      });
+      document.getElementById(clickedItem.dataset.id).classList.add('active');
+    });
+    homeclick.addEventListener('click', () => {
+      window.location.href = "../../index.html"
+    });
+  });
+}
+
+
+
+
+
+
+// スライドショー
+{
+  const images = [
+    'img/a.jpg',
+    'img/b.jpg',
+    'img/c.jpg',
+  ];
+  let currentIndex = 0;
+
+  const mainImage = document.getElementById('freedomeuwa-2');
+  mainImage.src = images[currentIndex];
+
+  images.forEach((image, index) => {
+    const img = document.createElement('img');
+    img.src = image;
+
+    const li = document.createElement('li');
+    if (index === currentIndex) {
+      li.classList.add('current');
+    }
+    li.addEventListener('click', () => {
+      mainImage.src = image;
+      const thumbnails = document.querySelectorAll('.thumbnails > li');
+      thumbnails[currentIndex].classList.remove('current');
+      currentIndex = index;
+      thumbnails[currentIndex].classList.add('current');
+    });
+
+    li.appendChild(img);
+    document.querySelector('.thumbnails').appendChild(li);
+  });
+
+  const next = document.getElementById('next');
+  next.addEventListener('click', () => {
+    let target = currentIndex + 1;
+    if (target === images.length) {
+      target = 0;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+  });
+
+  const prev = document.getElementById('prev');
+  prev.addEventListener('click', () => {
+    let target = currentIndex - 1;
+    if (target < 0) {
+      target = images.length - 1;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+  });
+}
